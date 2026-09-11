d7ddb96e3ad2ea6c568ae3aeed369039f1b682bf
syline <sylinelee@gmail.com>
2026-09-11 22:08:21 +0200

    feat(web): add checkout details type

---
+++ packages/web/src/types/CheckoutDetails.ts
@@ -0,0 +1,8 @@
+export interface CheckoutDetails {
+  firstName: string;
+  lastName?: string;
+  email: string;
+  phoneNumber?: string;
+  deliveryLocation: string;
+  targetDeliveryDate?: string;
+}
